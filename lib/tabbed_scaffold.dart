db90f105f30220336c27f005f7cd47b3930633af
Juan Diego Fajardo <jfajardo@mit.edu>
2019-06-02 18:09:16 -0600

    Actually export library!

--- lib/tabbed_scaffold.dart
+++ lib/tabbed_scaffold.dart
@@ -1,7 +1,3 @@
 library tabbed_scaffold;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:tabbed_scaffold/src/tabbed_scaffold.dart';
